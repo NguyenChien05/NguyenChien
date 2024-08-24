72898d1747462b6f97f89206cfaaf9e691f737d2
ChienFrancis <152265684+NguyenChien05@users.noreply.github.com>
2024-08-24 18:23:31 +0700

    Add files via upload

---
+++ Baitap.java
@@ -0,0 +1,26 @@
+
+package baitap;
+
+import java.util.Scanner;
+
+
+
+public class Baitap {
+
+    
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap a=");
+        a= sc.nextInt();
+        System.out.println("Nhap b=");
+        b= sc.nextInt();
+        System.out.println("a+b="+(a+b));
+        System.out.println("a-b="+(a-b));
+        System.out.println("a*b="+(a*b));
+        System.out.println("a/b=" +(a/b));
+        System.out.println("a%b="+ (a%b));
+        
+    }
+    
+}
